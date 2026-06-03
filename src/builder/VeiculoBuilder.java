e3a7342c6bebb6514e6ad4ac99908df32c3d16ca
Joao Victor Ribeiro Batista <228801047+Sayuuryi@users.noreply.github.com>
2026-06-03 14:19:04 -0300

    Atualizar atividade

--- src/builder/VeiculoBuilder.java
+++ src/builder/VeiculoBuilder.java
@@ -1,13 +1,9 @@
 package builder;
-
 import produto.Veiculo;
-
 public interface VeiculoBuilder {
-    
     void construirMotor();
     void construirTransmissao();
     void construirPintura();
     void construirRodas();
-    
     Veiculo getVeiculo();
 }
--- src/diretor/Diretor.java
+++ src/diretor/Diretor.java
@@ -1,20 +1,14 @@
 package diretor;
-
 import builder.VeiculoBuilder;
-
 public class Diretor {
-
     private VeiculoBuilder builder;
-
     public Diretor(VeiculoBuilder builder) {
         this.builder = builder;
     }
-
     public void construir() {
         builder.construirMotor();
         builder.construirTransmissao();
         builder.construirPintura();
         builder.construirRodas();
     }
-    
 }
